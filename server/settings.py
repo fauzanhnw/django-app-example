2d4cc4c10996b16911d9f7248eb89346c70ac282
Fauzan Hidayat <fauzanhidayat679@gmail.com>
2026-07-23 20:29:10 +0700

    change database to postgresql and configure poetry

--- server/settings.py
+++ server/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+config = Config(RepositoryEnv(BASE_DIR / "config" / ".env"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,8 +82,12 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT", default="5432"),
     }
 }
 
